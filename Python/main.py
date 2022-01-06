77bcaf8ac9edf23a71d5134fc3209814ec8ceb78
braga528 <braga528@gmail.com>
2022-01-06 09:54:07 -0600

    Se agregan constructures de python

--- Python/main.py
+++ Python/main.py
@@ -1,7 +1,9 @@
 from Car import Car
+from Account import Account
+
 if __name__ =="__main__":
     print("Hola mundo")
-    car = Car()
-    car.license = "ASM123"
-    car.driver = "Andres herrera"
+    
+    car = Car("AMS234", Account("Andres Herrera", "ANDA876"))
     print(vars(car))
+    print(vars(car.driver))
