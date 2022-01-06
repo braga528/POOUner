85b76d0161953c59a31a17da190c2e7a3c628f1d
braga528 <braga528@gmail.com>
2022-01-06 10:30:36 -0600

    Creando herencias en java y creando las clases uber black, pool y X

---
+++ Java/UberBlack.java
@@ -0,0 +1,12 @@
+class UberBlack extends Car {
+
+    String brand;
+    String model;
+
+    public UberBlack(String license, Account driver, String brand, String model){
+        super(license, driver);
+        this.brand = brand;
+        this.model = model;
+    }
+    
+}
